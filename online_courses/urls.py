8b9d7c59d71ad3a67347dc34c3923a7cec5950a9
Zalkarn <107370795+Zalkarn@users.noreply.github.com>
2024-12-11 08:02:24 +0500

    Delete online_courses directory

--- online_courses/urls.py
+++
@@ -1,8 +0,0 @@
-# project_name/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('courses.urls')),  # подключаем URLs приложения courses
-]
